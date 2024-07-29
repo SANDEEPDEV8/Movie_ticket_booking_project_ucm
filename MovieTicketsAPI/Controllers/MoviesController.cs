550b053dd682b9129d2bddf5641291581aae3457
ProgramSKAN <kandagatlas@gmail.com>
2024-07-29 17:08:08 -0500

    init

---
+++ MovieTicketsAPI/Controllers/MoviesController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieTicketsAPI.Data;
+using MovieTicketsAPI.Models;
+using MovieTicketsAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private MovieDbContext _dbContext;
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(MovieDbContext dbContext, IMovieRepository movieRepository)
+        {
+            _dbContext = dbContext;
+            _movieRepository = movieRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMovies(string sort, string keyword, int? pageNumber, int? pageSize)
+        {
+            var currentPageNumber = pageNumber ?? 1;
+            var currentPageSize = pageSize ?? 100;
+            var movieName = keyword ?? "";
+            var TotalCount = _dbContext.Movies.Where(q => q.Name.Contains(movieName)).Count();
+            var TotalPages = (int)Math.Ceiling(TotalCount / (double)currentPageSize);
+            var movies = from movie in _dbContext.Movies
+                        where movie.Name.Contains(movieName)
+                        select new
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                Duration = movie.Duration,
+                Language = movie.Language,
+                Rating = movie.Rating,
+                Genre = movie.Genre,
+                ImageUrl = movie.ImageUrl,
+                TotalPages = TotalPages,
+            };
+
+            switch (sort)
+            {
+                case "highest_rating":
+                    movies = movies.OrderByDescending(m => m.Rating);
+                    break;
+                case "lowest_rating":
+                    movies = movies.OrderBy(m => m.Rating);
+                    break;
+                case "longest_duration":
+                    movies = movies.OrderByDescending(m => m.Duration);
+                    break;
+                case "shortest_duration":
+                    movies = movies.OrderBy(m => m.Duration);
+                    break;
+                case "created_oldest":
+                    movies = movies.OrderBy(m => m.Id);
+                    break;
+                default:
+                    movies = movies.OrderByDescending(m => m.Id);
+                    break;
+            }
+
+            return Ok(movies.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMovie(int id)
+        {
+            var movie = await _movieRepository.GetMovie(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Search(string movieName)
+        {
+            var movies = from movie in _dbContext.Movies
+                         where movie.Name.StartsWith(movieName)
+                         select new
+                         {
+                             Id = movie.Id,
+                             Name = movie.Name,
+                             Duration = movie.Duration,
+                             Language = movie.Language,
+                             Rating = movie.Rating,
+                             Genre = movie.Genre,
+                             ImageUrl = movie.ImageUrl
+                         };
+            return Ok(movies);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Post([FromForm] Movie movieObj)
+        {
+            var guid = Guid.NewGuid();
+            var filePath = Path.Combine("wwwroot", guid + ".jpg");
+            if (movieObj.Image != null)
+            {
+                var fileStream = new FileStream(filePath, FileMode.Create);
+                movieObj.Image.CopyTo(fileStream);
+            }
+            movieObj.ImageUrl = filePath.Remove(0, 7);
+            _dbContext.Movies.Add(movieObj);
+            _dbContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromForm] Movie movieObj)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound("No data found with this Id");
+            }
+            else
+            {
+                var guid = Guid.NewGuid();
+                var filePath = Path.Combine("wwwroot", guid + ".jpg");
+                if (movieObj.Image != null)
+                {
+                    var fileStream = new FileStream(filePath, FileMode.Create);
+                    movieObj.Image.CopyTo(fileStream);
+                    movie.ImageUrl = filePath.Remove(0, 7);
+                }
+
+                movie.Name = movieObj.Name;
+                movie.Description = movieObj.Description;
+                movie.Language = movieObj.Language;
+                movie.Duration = movieObj.Duration;
+                movie.PlayingDate = movieObj.PlayingDate;
+                movie.PlayingTime = movie.PlayingTime;
+                movie.Rating = movieObj.Rating;
+                movie.Genre = movieObj.Genre;
+                movie.TrailorUrl = movieObj.TrailorUrl;
+                movie.TicketPrice = movieObj.TicketPrice;
+                _dbContext.SaveChanges();
+                return Ok("Successfully updated data");
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound("No data found with this Id");
+            }
+            else
+            {
+                _dbContext.Movies.Remove(movie);
+                _dbContext.SaveChanges();
+                return Ok("Successfully deleted data");
+            }
+        }
+    }
+}
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { CssBaseline, Typography, Container } from "@material-ui/core";
+import useStyles from "./FooterStyles";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {"Copyright © "}
+      Book Tickets {new Date().getFullYear()}
+      {"."} | Database theory and apps | Project
+    </Typography>
+  );
+}
+
+export default function StickyFooter() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
